7c205dae31c1591edd605b46361391eaed35fe0c
Andriejus <a.gorbunovas@gmail.com>
2020-05-11 15:55:30 +0300

    Pakeitimai

--- Zaidimas/Account.cs
+++ Zaidimas/Account.cs
@@ -9,21 +9,12 @@ namespace Zaidimas
     class Account
     {
         public string Vardas { get; set; }
-        public int? Lygis { get; set; }
-        public int Suma { get; set; } = 5000;
+        public int Lygis { get; set; }
+        public int Suma { get; set; } = 100;
         public Account(string vardas, int lygis)
         {
-            if (string.IsNullOrWhiteSpace(vardas))
-	{
-                 throw new Exception("Vardo laukas negali buti tuscias");
-	}
-           if (Lygis == null || Lygis < 1 || Lygis > 3)
-	{
-                throw new Exception("Neivestas arba neteisingai ivestas zaidimo lygis");
-	}
             Vardas = vardas;
             Lygis = lygis;
-        }
-        
+        }        
     }
 }

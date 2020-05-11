3349f8fe8173ca1c618df98aed34ab3a22b03de7
Vytautas <tumas.vytautas@gmail.com>
2020-05-11 13:13:53 +0300

    PirmasVariantas

---
+++ KonstravimasPrisijungimas/rankinisKonstravimas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstravimasPrisijungimas
+{
+    class rankinisKonstravimas
+    {
+        public string Dalis { get; set; }
+        public rankinisKonstravimas(string a)
+        {
+            Dalis = a;
+        }
+        public string UzklausaString(string b)
+        {
+
+            return $"SELECT Gyvybe, Kaina, Svoris from {Dalis} "
+            + $"WHERE {Dalis}='{b}' ";
+
+        }
+
+    }
+
+}
---
+++ Zaidimas/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaidimas
+{
+    class Account
+    {
+        public string Vardas { get; set; }
+        public int Lygis { get; set; }
+        public int Suma { get; set; } = 100;
+        public Account(string vardas, int lygis)
+        {
+            Vardas = vardas;
+            Lygis = lygis;
+        }
+
+    }
+}

c8b2d24f105ae3c1473c64f31600b14cba6ed5b2
Andriejus <a.gorbunovas@gmail.com>
2020-05-11 18:36:34 +0300

    Priimti pakeitimai2

--- Zaidimas/Account.cs
+++ Zaidimas/Account.cs
@@ -6,41 +6,46 @@ using System.Threading.Tasks;
 
 namespace Zaidimas
 {
-     public delegate void Kurimas();
+    public delegate void Kurimas();
     class Account
     {
-        Kurimas kurimoDelegatas = kv
+        Kurimas kurimoDelegatas = Kvietimas;
         public event Kurimas vardasIvestas;
 
         public string Vardas { get; set; }
-        public int Lygis { get; set; }
-        public int Suma { get; set; } = 100;
+        public int? Lygis { get; set; }
+        public int Suma { get; set; } = 5000;
         public Account(string vardas, int lygis)
         {
             if (string.IsNullOrWhiteSpace(vardas))
             {
                 throw new Exception("Vardo laukas negali buti tuscias");
             }
-           // if (Lygis == null || Lygis < 1 || Lygis > 3)
-           // {
-           //     throw new Exception("Neivestas arba neteisingai ivestas zaidimo lygis");
-           // }
-            Vardas = vardas;
-            Lygis = lygis;
+            if (Lygis == null || Lygis < 1 || Lygis > 3)
+            {
+                throw new Exception("Neivestas arba neteisingai ivestas zaidimo lygis");
+            }
+            vardas = vardas;
+            lygis = lygis;
 
             MetodasVardasIvestas();
         }
+        public void delegatoPaleidimas()
+        {
+            kurimoDelegatas();
+        }
+
         public virtual void MetodasVardasIvestas()
         {
 
             vardasIvestas?.Invoke();
         }
-        
-        
-        public void Kvietimas()
+
+
+        static public void Kvietimas()
         {
-            Veiksmas veiksmas = new Veiksmas(5);
-            veiksmas.Skaiciavimas(5);
+            Veiksmas veiksmas = new Veiksmas();
+            veiksmas.Skaiciavimas();
         }
 
     }

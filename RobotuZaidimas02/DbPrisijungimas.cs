9a37fab0bb4cdb3a8a63212ee062114586ff7e55
Vytautas <tumas.vytautas@gmail.com>
2020-05-12 17:08:32 +0300

    SudetaIVienaVieta2

---
+++ RobotuZaidimas02/DbPrisijungimas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace KonstravimasPrisijungimas
+{
+    class DbPrisijungimas
+    {
+        string connectionString =
+                "Data Source=localhost;Initial Catalog=Robots;Integrated Security=true";
+
+
+        public string queryString { get; set; }
+        public DbPrisijungimas(string a)
+        {
+            queryString = a;
+        }
+        public int Gyvybe { get; set; }
+        public int Kaina { get; set; }
+        public int Svoris { get; set; }
+        public string Dalis { get; set; }
+
+        public void Gyvbe()
+        {
+            int gyvybe = 0;
+            int kaina = 0;
+            int svoris = 0;
+            string dalis = null;
+
+            using (SqlConnection connection =
+                    new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        gyvybe = (int)reader[0];
+                        kaina = (int)reader[1];
+                        svoris = (int)reader[2];
+                        dalis = (string)reader[3];
+
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            Gyvybe = gyvybe;
+            Kaina = kaina;
+            Svoris = svoris;
+            Dalis = dalis;
+
+        }
+
+    }
+}
---
+++ RobotuZaidimas02/rankinisKonstravimas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstravimasPrisijungimas
+{
+    class rankinisKonstravimas
+    {
+        accoun
+        public string Dalis { get; set; }
+        public rankinisKonstravimas(string a)
+        {
+            Dalis = a;
+        }
+        public string UzklausaString(string b)
+        {
+
+            return $"SELECT Gyvybe, Kaina, Svoris from {Dalis} "
+            + $"WHERE {Dalis}='{b}' ";
+
+        }
+        public void RankinisIsrinkimas()
+    }
+
+}
